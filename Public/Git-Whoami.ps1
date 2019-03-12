6f4d0aab5555ced48cff976d00cf9190a0dcad8b
Brett Miller <brett@millerb.co.uk>
2019-03-12 10:40:06 +0000

    Updated git function to use approved verb

--- Public/Git-Whoami.ps1
+++
@@ -1,9 +0,0 @@
-function Git-Whoami {
-    $author = git config user.name
-    $email = git config user.email
-    
-    [pscustomobject]@{
-        Author = $author
-        Email = $email
-    }
-}
